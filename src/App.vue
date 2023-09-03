d9d818788558cfd78ed88420d0eddca572dd52b1
Filip Krunic <fkrunic9@gmail.com>
2023-09-02 18:03:24 -0700

    asset setup

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
 export default {
   data() {
     return {
-      bodyClass: ''
+      bodyClass: 'font-overpass'
     };
   },
   mounted() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    screens: {
+      desktop: '1440px'
+    },
+    colors: {
+      orange: 'hsl(25, 97%, 53%)',
+      white: 'hsl(0, 0%, 100%)',
+      'light-grey': 'hsl(217, 12%, 63%)',
+      'medium-grey': 'hsl(216, 12%, 54%)',
+      'dark-blue': 'hsl(213, 19%, 18%)',
+      'very-dark-blue': 'hsl(216, 12%, 8%)'
+    },
+    fontFamily: {
+      'overpass': 'Overpass, sans-serif'
+    },
     extend: {},
   },
   plugins: [],
